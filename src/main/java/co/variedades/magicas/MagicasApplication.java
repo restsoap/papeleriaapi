2ca4376fd3d388f53d0449733acb28706d65b815
Brayan Cañon <brayan13canon@gmail.com>
2025-05-05 22:39:06 -0500

    conexión a DB

---
+++ src/main/java/co/variedades/magicas/MagicasApplication.java
@@ -0,0 +1,13 @@
+package co.variedades.magicas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagicasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/variedades/magicas/MagicasApplicationTests.java
@@ -0,0 +1,13 @@
+package co.variedades.magicas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
